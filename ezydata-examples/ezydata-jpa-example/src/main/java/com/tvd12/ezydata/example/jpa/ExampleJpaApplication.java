f68cca464275a6b6c410bc71e8a63d16f2098bff
Dung Ta Van <itprono3@gmail.com>
2021-05-22 14:04:34 +0700

    remove examples

--- ezydata-examples/ezydata-jpa-example/src/main/java/com/tvd12/ezydata/example/jpa/ExampleJpaApplication.java
+++
@@ -1,9 +0,0 @@
-package com.tvd12.ezydata.example.jpa;
-
-import com.tvd12.ezyhttp.core.boot.EzyHttpApplicationBootstrap;
-
-public class ExampleJpaApplication {
-	public static void main(String[] args) throws Exception {
-		EzyHttpApplicationBootstrap.start(ExampleJpaApplication.class);	
-	}
-}
--- ezydata-examples/ezydata-mongo-example/src/main/java/com/tvd12/ezydata/example/mongo/repository/AuthorRepository.java
+++
@@ -1,9 +0,0 @@
-package com.tvd12.ezydata.example.mongo.repository;
-
-import com.tvd12.ezydata.example.mongo.entity.Author;
-import com.tvd12.ezydata.mongodb.EzyMongoRepository;
-import com.tvd12.ezyfox.database.annotation.EzyRepository;
-
-@EzyRepository
-public interface AuthorRepository extends EzyMongoRepository<Long, Author> {
-}
